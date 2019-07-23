ae822d15d70988042a486eba570eb115c2b8af00
Máté Mong <mongmate811@gmail.com>
2019-07-23 18:59:13 +0200

    big changes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -15,7 +15,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
 			return(
 				<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
 					<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-						<p onClick={() => onRouteChange('signedIn')} className='f3 link dim white underline pa3 pointer'>Sign In</p>
+						<p onClick={() => onRouteChange('signin')} className='f3 link dim white underline pa3 pointer'>Sign In</p>
 						<p onClick={() => onRouteChange('register')} className='f3 link dim white underline pa3 pointer'>Register</p>
 					</nav>
 				</article>
